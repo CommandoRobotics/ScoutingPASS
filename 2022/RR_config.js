2bafbc2542a79b0f2801db978c935f8669485ee9
AnthonyOfTulsa <AnthonyOfTulsa@gmail.com>
2022-04-07 22:32:55 -0500

    RR_config: Change to Green Country Regional
    Change code from OKC to Tulsa.

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022okok",
+        "defaultValue":"2022oktu",
         "required":"true",
         "disabled":"true"
       },
