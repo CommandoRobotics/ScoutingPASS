332816b57b1e3dd5bd43e5ec0d2df3dc80f4b758
AnthonyOfTulsa <AnthonyOfTulsa@gmail.com>
2025-03-10 09:17:52 -0500

    TBAInterface: Add new TBA key
    Add a new authentication key for this year.

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "2wWs4M2bPQUqvmrYkUevRF61VINm8JdlktlgUHEF9yuXgRkXYqgyMCW7fdxug05b";
 /**
  * Get list of teams in event
  *
